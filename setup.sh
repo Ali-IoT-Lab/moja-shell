3e0f562869d250d8689c33d8f5a6e6280403984a
Legendary <156110386@qq.com>
2018-08-27 11:17:43 +0800

    q

--- setup.sh
+++ setup.sh
@@ -12,24 +12,26 @@ fi
 apt-get -y install expect
 
 logPath="/var/tmp/remote-terminal-client-logs"
+logsTarPath="/var/tmp/remote-terminal-client-logs-tar"
+
 appPath="/home/moja/remote-terminal-client/app.js"
 pm2Path="/home/moja/nodejs/bin/pm2"
 npmPath="/home/moja/nodejs/bin/npm"
 clientPath="/home/moja/remote-terminal-client"
-
-hostName="47.98.253.35"
+deamonPath="sh /home/moja/remote-terminal-client/deamon/deamon.sh"
+hostName="terminal.moja.studio"
 port=3000
 
 #设置开机启动pm2
-autoStart="su - moja -c '$pm2Path start $appPath --log-type json --merge-logs --log-date-format=\"YYYY-MM-DD HH:mm:ss Z\" -o $logPath/stdout.json -e $logPath/stderr.json'"
-if [ ! -f "/etc/init.d/rc.local" ]; then
-  touch /etc/init.d/rc.local
+autoStart="sudo runuser -l moja -c '$pm2Path start $appPath --log-type json --merge-logs --log-date-format=\"YYYY-MM-DD HH:mm:ss Z\" -o $logPath/stdout.json -e $logPath/stderr.json'"
+if [ ! -f "/etc/rc.local" ]; then
+  touch /etc/rc.local
 fi
-
-chmod 755 /etc/init.d/rc.local
-sed -i '/${autoStart}/d' /etc/init.d/rc.local
-echo "${autoStart}" >> /etc/init.d/rc.local
-
+chmod 755 /etc/rc.local
+sed -i '/exit 0/d' /etc/rc.local
+sed -i '/--log-type json --merge-logs --log-date-format=\"YYYY-MM-DD HH:mm:ss Z\"/d' /etc/rc.local
+echo "${autoStart}" >> /etc/rc.local
+echo "exit 0" >> /etc/rc.local
 #获取nodejs版本号
 if [ $# -eq 0 ] ; then
 	VERSION=8.11.3
@@ -58,7 +60,6 @@ if ! id moja
 then
 useradd -s /bin/bash -d /home/moja  -U moja -p 123456 -m
 fi
-chown -R moja:moja /home/moja
 chmod 777 /home/moja
 mkdir -p /home/moja/nodejs
 
@@ -70,7 +71,7 @@ runuser -l moja -c "$npmPath  uninstall -g pm2"
 runuser -l moja -c "rm /home/moja/.pm2 -rf"
 runuser -l moja -c "rm /home/moja/nodejs -rf"
 
-mkdir -p /home/moja/nodejs
+runuser -l moja -c "mkdir -p /home/moja/nodejs"
 
 runuser -l moja -c "echo `which pm2` |  while read line
 do
@@ -92,48 +93,33 @@ tar -xJf /home/moja/${verName} --no-wildcards-match-slash --anchored \
      --exclude */CHANGELOG.md --exclude */LICENSE --exclude */README.md \
      --strip 1 -C /home/moja/nodejs
 
+
+sed -i '/moja/d' /home/moja/.profile
+
 echo "export PATH=`echo $PATH |sed 's/:\/home\/moja\/nodejs\/bin\///g'`:/home/moja/nodejs/bin/" >> /home/moja/.profile
 source /home/moja/.profile
-wget -O $clientPath.tar.gz http://$hostName:$port/api/remote_terminal/getterminaltar
+
+wget -O $clientPath.tar.gz http://$hostName/api/remote_terminal/getterminaltar
 tar -xz -f $clientPath.tar.gz
+
 sleep 2
+
 $npmPath install --prefix $clientPath node-pty
 $npmPath install --prefix $clientPath socket.io-client@2.1.1
 mv /var/tmp/`basename $0` $clientPath/bin/`basename $0`
 
 $npmPath install pm2@latest -g
 
+chown -R moja:moja /home/moja
+
 runuser -l moja -c "$pm2Path start $appPath --log-type json --merge-logs --log-date-format=\"YYYY-MM-DD HH:mm:ss Z\" -o $logPath/stdout.json -e $logPath/stderr.json"
 runuser -l moja -c "$pm2Path save"
-
 runuser -l moja -c "crontab -r"
-runuser -l moja -c "(echo '* * * * */1 $pm2Path flush' ;crontab -l) | crontab"
-runuser -l moja -c "(echo '* * */1 * * tar czvfP /var/tmp/\$(date +\"\%Y-\%m-\%d\")-log.tar $logPath >>/dev/null' ;crontab -l) |crontab"
-runuser -l moja -c "(echo '*/1 * * * * if ps -ef |grep -v grep|grep $logPath == \"\";then $pm2Path start $appPath --log-type json --merge-logs --log-date-format=\"YYYY-MM-DD HH:mm:ss Z\" -o $logPath/stdout.json -e $logPath/stderr.json; fi';crontab -l) | crontab"
+runuser -l moja -c "(echo '*/1 * * * * $deamonPath' ;crontab -u moja -l) | crontab -u moja -"
+runuser -l moja -c "(echo '0 0 * * */1 $pm2Path flush;echo > $logPath/stderr.json;echo > $logPath/stdout.json;rm $logsTarPath/* -rf';crontab -u moja -l) | crontab -u moja -"
+runuser -l moja -c "(echo '0 0 */1 * * if [ ! -d $logsTarPath ]; then mkdir -p  $logsTarPath; fi;tar czvfP $logsTarPath/`date +\%Y-\%m-\%d`-log.tar $logPath >>/dev/null;echo > $logPath/stdout.json;echo > $logPath/stderr.json' ;crontab -u moja -l) | crontab -u moja -"
 
-wget -O /home/moja/su_moja http://$hostName:$port/getshell/su_moja.sh?type=1
+/etc/init.d/cron restart
 
+wget -O /home/moja/su_moja http://$hostName/getshell/su_moja.sh?type=1
 #/usr/bin/expect /home/moja/su_moja
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
